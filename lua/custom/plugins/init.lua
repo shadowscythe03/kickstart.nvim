b9e88cc34f3aee12f45d00e3cb475c574e506615
abhiroop <abhiroop.ch1508@gmail.com>
2025-12-04 23:41:58 +0530

    Added theme in custom config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+        "scottmckendry/cyberdream.nvim",
+        priority = 1000, -- load before other plugins
+        config = function()
+        require("cyberdream").setup({
+            transparent = true,
+            italic_comments = true,
+        })
+        vim.cmd.colorscheme("cyberdream")
+        end,
+    },
+
+}
