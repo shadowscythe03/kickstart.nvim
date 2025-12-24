51907f6fbd472143583ef3e1175fe95bf15990d7
abhiroop <abhiroop.ch1508@gmail.com>
2025-12-24 12:54:07 +0530

    added onedark

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -55,4 +55,15 @@ return {
       require('kanagawa').setup()
     end,
   },
+  -- Using Lazy
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
+      require('onedark').load()
+    end
+  }
 }
